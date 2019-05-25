4d76d9959304f42931447509c795003c49d8d8a1
MaxLye <maxlye93@gmail.com>
2019-05-25 15:00:18 +0800

    send request to server done

--- JS/devices.js
+++ JS/devices.js
@@ -1,21 +1,22 @@
 function loadDevices(area) {
-  //var doc="";
   $.get("http://192.168.31.16:8000/loaditems/" + area, function (data) {
     var doc = "";
     console.log(data);
     var itemList = JSON.parse(data);
     for (var i = 1; i <= itemList.length; i++) {
-      var onOff = "<input type=\"checkbox\" onclick=\"turnSwitch("+itemList[i - 1]["ID"]+");\">";
+      var onOff = "<input type=\"checkbox\" id="+itemList[i - 1]["EName"]+" onclick=\"turnSwitch('"+itemList[i - 1]["EName"]+"','"+area+"');\">";
       if (itemList[i - 1]["Status"] == 1) {
-        onOff = "<input type=\"checkbox\" onclick=\"turnSwitch("+itemList[i - 1]["ID"]+");\" checked>";
+        onOff = "<input type=\"checkbox\" id="+itemList[i - 1]["EName"]+" onclick=\"turnSwitch('"+itemList[i - 1]["EName"]+"','"+area+"');\" checked>";
       }
       doc += `<tr><td>` + i + `.</td><td>` + itemList[i - 1]["Name"] + `</td><td><label class="switch">` + onOff + `<span class="slider round"></span></label></td></tr>`
     }
     $("#devices").html(doc);
   });
-  //document.getElementById("devices").innerHTML = doc;
 }
 
-function turnSwitch(id){
-  
+function turnSwitch(EName,area){
+  var status = $('#'+EName).prop('checked')?"on":"off";
+  $.post("http://192.168.31.16:8000/action/"+area+"/"+EName+"/"+status,{"token":"abc123"}, function(data){
+    console.log(data);
+  });
 }
